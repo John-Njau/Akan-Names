34fc5bf47bac1f34124d6eb08da299ae100da085
John <john.njoroge@student.moringaschool.com>
2022-03-06 10:59:20 +0300

    ft:Created arrays to hold the akan names

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// Array to hold the Akan names.
+var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw','Kofi', 'Kwame'];
+var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
