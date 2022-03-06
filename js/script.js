0a5bec4b6889daf276204e8134854fbc9505b2da
John <john.njoroge@student.moringaschool.com>
2022-03-06 11:02:25 +0300

    ft:form validation using if statement

--- js/script.js
+++ js/script.js
@@ -17,4 +17,27 @@ document.querySelector('#submit').addEventListener('click', ()=>{
     // var birthDay = new Date('year' + '-' + 'month' + '-' +'date')
     let dateStr = birthDay.toDateString();
     let birthDate = birthDay.getDay();
+
+
+    // Form validation
+    if (yob === ""){
+        window.alert("Enter your year of birth.");
+        yob.focus()
+        return false;
+    }
+    else if (month === ""){
+        window.alert("Enter your month of birth.");
+        month.focus()
+        return false;
+    }
+    else if (dob === ""){
+        window.alert("Enter your date of birth.");
+        dob.focus()
+        return false;
+    }
+    else if (gender == "select-gender"){
+        window.alert("Select your gender.");
+        gender.focus();
+        return false;
+    }
 });
